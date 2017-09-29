59ae89586b256bbf34df29339aed757fef6704ce
aniket umbc <aniket.umbc@gmail.com>
2017-09-29 12:12:28 +0530

    'add-connect-disconnect'

--- Server/server.js
+++ Server/server.js
@@ -1,23 +1,31 @@
 const path=require('path');
+const http=require('http');
 const publicPath= path.join(__dirname,'../public');
 const express=require('express'); //middleware express
-var app=express();//middleware
-app.use(express.static(publicPath));
+const sokectIO=require('socket.io');
 const port=process.env.PORT || 3000;
+var app=express();//middleware
+var server=http.createServer(app);
+var io=sokectIO(server);
+
+
+app.use(express.static(publicPath));
+
+io.on('connection',(socket)=>{
+console.log('New User Connected');
+
+socket.on('disconnect',(socket)=>{
+    console.log('User disconnected');
+    
+});
+});
 
 
 
 
 
 
-
-
-
-
-
-
-
-app.listen(port,()=>{
+server.listen(port,()=>{
     
         console.log(`Hello console Serverup ${port}` );
     });
