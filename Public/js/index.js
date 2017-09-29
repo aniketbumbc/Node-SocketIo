71788d852cd03724b8f6315b81e35f78046420a4
aniket umbc <aniket.umbc@gmail.com>
2017-09-29 12:54:31 +0530

    'create-event-message'

---
+++ Public/js/index.js
@@ -0,0 +1,28 @@
+var socket=io();
+socket.on('connect',function(){
+    console.log('Connected to Server');
+
+    // socket.emit('createEmail',{
+    //     to:'aniket@umbc.edu',
+    //     text:'how are you today'
+    // });
+
+
+    socket.emit('createMessage',{
+        from:'Bunny',
+        text:'Hello How are you',
+        });
+});
+
+
+
+
+
+socket.on('disconnect', function(){
+    console.log('Disconnected to Server');
+
+});
+
+socket.on('newMessage',function (message){
+console.log('newMessage',message);
+})
--- Server/server.js
+++ Server/server.js
@@ -11,20 +11,43 @@ var io=sokectIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection',(socket)=>{
+io.on('connection',(socket)=>{     //connection event to client 
 console.log('New User Connected');
 
+// socket.emit('newEmail',{
+//     from:'Mikeexpm.com',
+//     text:'what is going on',
+//     createAt:12.30
+// });
+
+// socket.on('createEmail',(newEmail)=>{
+//     console.log('createEmail',newEmail);
+// })
+
+socket.emit('newMessage',{
+from:'Bunny',
+text:'Hello How are you',
+createAt:1.00
+});
+
+// socket.on('createChat',(newChat)=>{
+//     console.log('createChat',newChat);
+// })
+
+socket.on('createMessage', (message) =>{
+    console.log('Create Message',message);
+
+});
+
+
+
+
+
+
 socket.on('disconnect',(socket)=>{
-    console.log('User disconnected');
-    
+    console.log('User disconnected');    
 });
 });
-
-
-
-
-
-
 server.listen(port,()=>{
     
         console.log(`Hello console Serverup ${port}` );
