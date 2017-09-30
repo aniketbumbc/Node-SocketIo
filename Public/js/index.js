45ca809be42d625dccdf23fd3b68d91c8c8cf883
aniket umbc <aniket.umbc@gmail.com>
2017-09-30 12:03:11 +0530

    'add-form-message-incoming-message'

--- Public/js/index.js
+++ Public/js/index.js
@@ -25,13 +25,22 @@ socket.on('disconnect', function(){
 
 socket.on('newMessage',function (message){
 console.log('newMessage',message);
+var li=jQuery('<li></li>')
+li.text(`${message.from}:${message.text}`)
+jQuery('#message').append(li);
 })
+
+jQuery('#message-form').on('submit',function(e){
+e.preventDefault();
+
 
 socket.emit('createMessage',{
 
-    from:"Frank",
-    text:"hello"
+    from:'User',
+    text:jQuery('[name=message]').val()
 
-},function(data){
-    console.log('Got Message',data.text);
+},function(){
+
+
 })
+});
